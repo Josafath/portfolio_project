238676c310680dd3a143ad79197a51d0561ba5f9
Josafath <kevincc@live.com>
2022-01-31 16:12:01 -0600

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-
+import '../styles/style.css'
+import '../styles/styles1.css'
+import '../styles/style_contact.css'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+        <Component {...pageProps} />
+        );
 }
 
 export default MyApp
