dffaa4146dd75e78bec9192235e77b7b5bfee533
Josafath <kevincc@live.com>
2024-03-16 09:08:56 -0600

    Delete the old structure of the page

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/style.css'
-import '../styles/style_404.css'
-function MyApp({ Component, pageProps }) {
-  return (
-        <Component {...pageProps} />
-        );
-}
-
-export default MyApp
